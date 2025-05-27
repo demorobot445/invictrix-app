d13ba9e46484d2854b9efac81c4b5db0dd96ce90
Ahmed Khan <demo11website@gmail.com>
2025-05-27 18:52:54 +0500

    Update godaddy-email.ts

--- pages/api/godaddy-email.ts
+++ pages/api/godaddy-email.ts
@@ -44,10 +44,13 @@ export default async function handler(
   try {
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: "Email sent successfully" });
-  } catch (error: any) {
+  } catch (error) {
     console.error("Email sending failed", error);
     res
       .status(500)
-      .json({ message: "Failed to send email", error: error.message });
+      .json({
+        message: "Failed to send email",
+        error: (error as { message: string }).message,
+      });
   }
 }
