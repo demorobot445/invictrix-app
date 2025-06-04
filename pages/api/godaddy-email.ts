a8b9108d75fadec06b9c041a71af99d55238ba77
Ahmed Khan <demo11website@gmail.com>
2025-06-04 15:25:54 +0500

    Update godaddy-email.ts

--- pages/api/godaddy-email.ts
+++ pages/api/godaddy-email.ts
@@ -5,26 +5,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "POST")
-    return res.status(405).json({ message: "Method not allowed" });
+  if (req.method === "POST") {
+    const data = req.body;
 
-  const data = req.body;
+    const transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.GODADDY_EMAIL_USER,
+        pass: process.env.GODADDY_EMAIL_PASSWORD,
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.GODADDY_EMAIL_USER,
-      pass: process.env.GODADDY_EMAIL_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.GODADDY_EMAIL_USER,
-    to: process.env.RECEIVE_EMAIL,
-    subject: "New Invictrix Connection Request",
-    html: `
+    const mailOptions = {
+      from: process.env.GODADDY_EMAIL_USER,
+      to: process.env.RECEIVE_EMAIL,
+      subject: "New Invictrix Connection Request",
+      html: `
       <h3>New Submission</h3>
       <ul>
           <li><strong>Salutation:</strong> ${data.salutation}</li>
@@ -39,18 +37,21 @@ export default async function handler(
           <li><strong>Tell Us About Yourself:</strong> ${data.about}</li>
       </ul>
     `,
-  };
+    };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: "Email sent successfully" });
-  } catch (error) {
-    console.error("Email sending failed", error);
-    res
-      .status(500)
-      .json({
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Email sent successfully" });
+    } catch (error) {
+      console.error("Email sending failed", error);
+      res.status(500).json({
         message: "Failed to send email",
         error: (error as { message: string }).message,
       });
+    }
+  } else if (req.method === "GET") {
+    return res.status(200).json({ message: "GET request received" });
+  } else {
+    return res.status(405).json({ message: "Method not allowed" });
   }
 }
